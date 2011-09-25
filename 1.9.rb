3f67726bd28e816b4d910d0ec70582bd90f2d06d
Bethany Nowviskie <bethany@virginia.edu>
2011-09-24 23:49:28 -0400

    Fibonacci? Fuhgeddaboudit.

---
+++ 1.9.rb
@@ -0,0 +1,9 @@
+puts "Ready to find the sum of all the even-valued terms in the Fibonacci sequence which do not exceed 4 million?"
+answer = gets.chomp.to_s
+
+if answer == "yes"
+  puts "Go away, Waynebot."
+elsif answer == "no"
+  puts "Correct. There's a reason we majored in English."
+end
+
