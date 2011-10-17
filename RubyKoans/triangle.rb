5bb155c2c8d48fbe7a37077c8fa8a6f46bc62965
Bethany Nowviskie <bethany@virginia.edu>
2011-10-16 22:54:35 -0400

    triangle koan & such

--- RubyKoans/triangle.rb
+++ RubyKoans/triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a + b <= c
+  return :equilateral if a == c
+  return :isosceles if a == b or b == c
+  return :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
