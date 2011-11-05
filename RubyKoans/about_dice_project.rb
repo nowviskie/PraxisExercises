33c12733c182780a9259becd91ccd4fbc3111fa6
Bethany Nowviskie <bethany@virginia.edu>
2011-11-05 10:36:39 -0400

    path of enlightenment is a yellow brick road

--- RubyKoans/about_dice_project.rb
+++ RubyKoans/about_dice_project.rb
@@ -2,9 +2,12 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  def roll(num)
+    @values = (1..num).map { rand(6) + 1 }
+  end
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
